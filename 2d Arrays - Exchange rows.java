c0b10b53c1ba72aa3e687349266099140b1472c1
Mostafijur28 <mostafijur.riju@gmail.com>
2025-07-05 23:02:46 +0530

    2d arrays codes

---
+++ 2d Arrays - Exchange rows.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int mat[][] = new int[n][m];
+        
+        for(int i=0; i<n; i++){
+            
+            for(int j=0; j<m; j++){
+                
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            int temp = mat[0][i];
+            mat[0][i] = mat[n-1][i];
+            mat[n-1][i] = temp;
+        }
+        
+        for(int i=0; i<n; i++){
+            
+            for(int j=0; j<m; j++){
+                
+                System.out.print(mat[i][j] + " ");
+            }
+            
+            System.out.println();
+        }
+        
+    }
+}
